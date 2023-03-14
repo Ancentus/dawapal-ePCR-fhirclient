f56a1cbd56c5da56325678d6d4cc459d89abdf1e
Ancentus <mancentus7@gmail.com>
2023-03-14 23:49:32 +0300

    Initialize spring app

---
+++ src/main/java/com/dawapal/epcr/DawapalECprHapiFhirClientApplication.java
@@ -0,0 +1,13 @@
+package com.dawapal.epcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawapalECprHapiFhirClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawapalECprHapiFhirClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawapal/epcr/DawapalECprHapiFhirClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawapal.epcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawapalECprHapiFhirClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
