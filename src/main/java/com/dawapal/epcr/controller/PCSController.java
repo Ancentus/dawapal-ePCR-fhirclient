dd922152c7a6c635b1a42dc08b03e5ac44fb5bd1
Ancentus <mancentus7@gmail.com>
2023-03-15 22:21:21 +0300

    Create resource for weight and Print for testing

--- src/main/java/com/dawapal/epcr/controller/PCSController.java
+++ src/main/java/com/dawapal/epcr/controller/PCSController.java
@@ -31,15 +31,17 @@ public class PCSController {
 
     // Create a client
     IGenericClient client = ctx.newRestfulGenericClient("https://hapi.fhir.org/baseR4");
-    
-    /**s
+
+    /**
+     * s
+     * 
      * @param pcs
      * @return ResponseEntity<String>
-     *     gets pcs data from client form and creates a pcs composition resource
+     *         gets pcs data from client form and creates a pcs composition resource
      */
 
     @PostMapping("/pcs/create")
-    public ResponseEntity<String> createPCS(ParamedicineCareSummary pcs){
+    public ResponseEntity<String> createPCS(ParamedicineCareSummary pcs) {
         // Create Patient Resource
         Patient patient = new Patient();
         // TODO: populate patient
@@ -61,8 +63,9 @@ public class PCSController {
 
         // Set all vital signs to this category
         CodeableConcept vital_cat = new CodeableConcept();
-        vital_cat.addCoding().setCode("vital-signs").setSystem("http://terminology.hl7.org/CodeSystem/observation-category");
-        
+        vital_cat.addCoding().setCode("vital-signs")
+                .setSystem("http://terminology.hl7.org/CodeSystem/observation-category");
+
         // Set all vital signs to this effective dateTime
         DateTimeType vitals_dt = new DateTimeType(pcs.getVital_dateTime());
 
@@ -76,7 +79,7 @@ public class PCSController {
             ob_ecg.setCode(ecg_code);
             ob_ecg.setValue(new StringType(pcs.getVital_ecg()));
             ob_ecg.setEffective(vitals_dt);
-            
+
             ob_ecg.setSubject(patientRef);
             ob_ecg.setEncounter(encounterRef);
         }
@@ -91,7 +94,7 @@ public class PCSController {
             ob_sbp.setCode(sbp_code);
             ob_sbp.setValue(new StringType(pcs.getVital_sbp()));
             ob_sbp.setEffective(vitals_dt);
-            
+
             ob_sbp.setSubject(patientRef);
             ob_sbp.setEncounter(encounterRef);
         }
@@ -106,7 +109,7 @@ public class PCSController {
             ob_dbp.setCode(dbp_code);
             ob_dbp.setValue(new StringType(pcs.getVital_dbp()));
             ob_dbp.setEffective(vitals_dt);
-            
+
             ob_dbp.setSubject(patientRef);
             ob_dbp.setEncounter(encounterRef);
         }
@@ -121,7 +124,7 @@ public class PCSController {
             ob_mean_ap.setCode(mean_ap_code);
             ob_mean_ap.setValue(new StringType(pcs.getVital_mean_ap()));
             ob_mean_ap.setEffective(vitals_dt);
-            
+
             ob_mean_ap.setSubject(patientRef);
             ob_mean_ap.setEncounter(encounterRef);
         }
@@ -136,7 +139,7 @@ public class PCSController {
             ob_heart_rate.setCode(heart_rate_code);
             ob_heart_rate.setValue(new StringType(pcs.getVital_heart_rate()));
             ob_heart_rate.setEffective(vitals_dt);
-            
+
             ob_heart_rate.setSubject(patientRef);
             ob_heart_rate.setEncounter(encounterRef);
         }
@@ -151,7 +154,7 @@ public class PCSController {
             ob_pulse_oximetry.setCode(pulse_oximetry_code);
             ob_pulse_oximetry.setValue(new StringType(pcs.getVital_pulse_oximetry()));
             ob_pulse_oximetry.setEffective(vitals_dt);
-            
+
             ob_pulse_oximetry.setSubject(patientRef);
             ob_pulse_oximetry.setEncounter(encounterRef);
         }
@@ -166,7 +169,7 @@ public class PCSController {
             ob_pulse_rhythm.setCode(pulse_rhythm_code);
             ob_pulse_rhythm.setValue(new StringType(pcs.getVital_pulse_rhythm()));
             ob_pulse_rhythm.setEffective(vitals_dt);
-            
+
             ob_pulse_rhythm.setSubject(patientRef);
             ob_pulse_rhythm.setEncounter(encounterRef);
         }
@@ -181,7 +184,7 @@ public class PCSController {
             ob_respiratory_rate.setCode(respiratory_rate_code);
             ob_respiratory_rate.setValue(new StringType(pcs.getVital_respiratory_rate()));
             ob_respiratory_rate.setEffective(vitals_dt);
-            
+
             ob_respiratory_rate.setSubject(patientRef);
             ob_respiratory_rate.setEncounter(encounterRef);
         }
@@ -196,13 +199,13 @@ public class PCSController {
             ob_respiratory_effort.setCode(respiratory_effort_code);
             ob_respiratory_effort.setValue(new StringType(pcs.getVital_respiratory_effort()));
             ob_respiratory_effort.setEffective(vitals_dt);
-            
+
             ob_respiratory_effort.setSubject(patientRef);
             ob_respiratory_effort.setEncounter(encounterRef);
         }
 
-         // End Title Carbon Dioxide (ETCO2)
-         if (!pcs.getVital_etco2().isEmpty()) {
+        // End Title Carbon Dioxide (ETCO2)
+        if (!pcs.getVital_etco2().isEmpty()) {
             Observation ob_etco2 = new Observation();
             ob_etco2.setStatus(ObservationStatus.FINAL);
             ob_etco2.addCategory(vital_cat);
@@ -211,7 +214,7 @@ public class PCSController {
             ob_etco2.setCode(etco2_code);
             ob_etco2.setValue(new StringType(pcs.getVital_etco2()));
             ob_etco2.setEffective(vitals_dt);
-            
+
             ob_etco2.setSubject(patientRef);
             ob_etco2.setEncounter(encounterRef);
         }
@@ -226,7 +229,7 @@ public class PCSController {
             ob_co.setCode(co_code);
             ob_co.setValue(new StringType(pcs.getVital_co()));
             ob_co.setEffective(vitals_dt);
-            
+
             ob_co.setSubject(patientRef);
             ob_co.setEncounter(encounterRef);
         }
@@ -241,7 +244,7 @@ public class PCSController {
             ob_glucose_level.setCode(glucose_level_code);
             ob_glucose_level.setValue(new StringType(pcs.getVital_glucose_level()));
             ob_glucose_level.setEffective(vitals_dt);
-            
+
             ob_glucose_level.setSubject(patientRef);
             ob_glucose_level.setEncounter(encounterRef);
         }
@@ -256,7 +259,7 @@ public class PCSController {
             ob_temperature.setCode(temperature_code);
             ob_temperature.setValue(new StringType(pcs.getVital_temperature()));
             ob_temperature.setEffective(vitals_dt);
-            
+
             ob_temperature.setSubject(patientRef);
             ob_temperature.setEncounter(encounterRef);
         }
@@ -271,7 +274,7 @@ public class PCSController {
             ob_pain_scale_score.setCode(pain_scale_score_code);
             ob_pain_scale_score.setValue(new StringType(pcs.getVital_pain_scale_score()));
             ob_pain_scale_score.setEffective(vitals_dt);
-            
+
             ob_pain_scale_score.setSubject(patientRef);
             ob_pain_scale_score.setEncounter(encounterRef);
         }
@@ -286,7 +289,7 @@ public class PCSController {
             ob_stroke_scale_score.setCode(stroke_scale_score_code);
             ob_stroke_scale_score.setValue(new StringType(pcs.getVital_stroke_scale_score()));
             ob_stroke_scale_score.setEffective(vitals_dt);
-            
+
             ob_stroke_scale_score.setSubject(patientRef);
             ob_stroke_scale_score.setEncounter(encounterRef);
         }
@@ -301,7 +304,7 @@ public class PCSController {
             ob_coma_score_eye.setCode(coma_score_eye_code);
             ob_coma_score_eye.setValue(new StringType(pcs.getVital_coma_score_eye()));
             ob_coma_score_eye.setEffective(vitals_dt);
-            
+
             ob_coma_score_eye.setSubject(patientRef);
             ob_coma_score_eye.setEncounter(encounterRef);
         }
@@ -316,7 +319,7 @@ public class PCSController {
             ob_coma_score_verbal.setCode(coma_score_verbal_code);
             ob_coma_score_verbal.setValue(new StringType(pcs.getVital_coma_score_verbal()));
             ob_coma_score_verbal.setEffective(vitals_dt);
-            
+
             ob_coma_score_verbal.setSubject(patientRef);
             ob_coma_score_verbal.setEncounter(encounterRef);
         }
@@ -331,7 +334,7 @@ public class PCSController {
             ob_coma_score_motor.setCode(coma_score_motor_code);
             ob_coma_score_motor.setValue(new StringType(pcs.getVital_coma_score_motor()));
             ob_coma_score_motor.setEffective(vitals_dt);
-            
+
             ob_coma_score_motor.setSubject(patientRef);
             ob_coma_score_motor.setEncounter(encounterRef);
         }
@@ -346,7 +349,7 @@ public class PCSController {
             ob_coma_score_qualifier.setCode(coma_score_qualifier_code);
             ob_coma_score_qualifier.setValue(new StringType(pcs.getVital_coma_score_qualifier()));
             ob_coma_score_qualifier.setEffective(vitals_dt);
-            
+
             ob_coma_score_qualifier.setSubject(patientRef);
             ob_coma_score_qualifier.setEncounter(encounterRef);
         }
@@ -361,7 +364,7 @@ public class PCSController {
             ob_coma_score_total.setCode(coma_score_total_code);
             ob_coma_score_total.setValue(new StringType(pcs.getVital_coma_score_total()));
             ob_coma_score_total.setEffective(vitals_dt);
-            
+
             ob_coma_score_total.setSubject(patientRef);
             ob_coma_score_total.setEncounter(encounterRef);
         }
@@ -376,7 +379,7 @@ public class PCSController {
             ob_reperfusion_checklist.setCode(reperfusion_checklist_code);
             ob_reperfusion_checklist.setValue(new StringType(pcs.getVital_reperfusion_checklist()));
             ob_reperfusion_checklist.setEffective(vitals_dt);
-            
+
             ob_reperfusion_checklist.setSubject(patientRef);
             ob_reperfusion_checklist.setEncounter(encounterRef);
         }
@@ -391,27 +394,12 @@ public class PCSController {
             ob_apgar.setCode(apgar_code);
             ob_apgar.setValue(new StringType(pcs.getVital_apgar()));
             ob_apgar.setEffective(vitals_dt);
-            
+
             ob_apgar.setSubject(patientRef);
             ob_apgar.setEncounter(encounterRef);
         }
 
-         // Revised Trauma Score
-         if (!pcs.getVital_revised_trauma_score().isEmpty()) {
-            Observation ob_revised_trauma_score = new Observation();
-            ob_revised_trauma_score.setStatus(ObservationStatus.FINAL);
-            ob_revised_trauma_score.addCategory(vital_cat);
-            CodeableConcept revised_trauma_score_code = new CodeableConcept();
-            revised_trauma_score_code.addCoding().setDisplay("Revised Trauma Score");
-            ob_revised_trauma_score.setCode(revised_trauma_score_code);
-            ob_revised_trauma_score.setValue(new StringType(pcs.getVital_revised_trauma_score()));
-            ob_revised_trauma_score.setEffective(vitals_dt);
-            
-            ob_revised_trauma_score.setSubject(patientRef);
-            ob_revised_trauma_score.setEncounter(encounterRef);
-        }
-
-        // Estimated Body Weight in Kilograms
+        // Revised Trauma Score
         if (!pcs.getVital_revised_trauma_score().isEmpty()) {
             Observation ob_revised_trauma_score = new Observation();
             ob_revised_trauma_score.setStatus(ObservationStatus.FINAL);
@@ -421,11 +409,29 @@ public class PCSController {
             ob_revised_trauma_score.setCode(revised_trauma_score_code);
             ob_revised_trauma_score.setValue(new StringType(pcs.getVital_revised_trauma_score()));
             ob_revised_trauma_score.setEffective(vitals_dt);
-            
+
             ob_revised_trauma_score.setSubject(patientRef);
             ob_revised_trauma_score.setEncounter(encounterRef);
         }
 
+        // Estimated Body Weight in Kilograms
+        if (!pcs.getVital_body_weight().isEmpty()) {
+            Observation ob_body_weight = new Observation();
+            ob_body_weight.setStatus(ObservationStatus.FINAL);
+            ob_body_weight.addCategory(vital_cat);
+            CodeableConcept body_weight_code = new CodeableConcept();
+            body_weight_code.addCoding().setDisplay("Estimated Body Weight in Kilograms");
+            ob_body_weight.setCode(body_weight_code);
+            ob_body_weight.setValue(new StringType(pcs.getVital_body_weight()));
+            ob_body_weight.setEffective(vitals_dt);
+
+            ob_body_weight.setSubject(patientRef);
+            ob_body_weight.setEncounter(encounterRef);
+
+            // Print the Resource
+            String string = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(ob_body_weight);
+            System.out.println(string);
+        }
 
         return new ResponseEntity<>("Work in progress: ", HttpStatus.OK);
     }
